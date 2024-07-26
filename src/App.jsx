ff20bd817b9f78862acbe5fde5143a362058fd46
Omar Yasser <o.yasser.n@gmail.com>
2024-07-26 14:49:11 +0300

    added redux/store component to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import LoginWithFacebook from './components/LoginWithFacebook/LoginWithFacebook'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import LoginWithFacebook from "./components/LoginWithFacebook/LoginWithFacebook";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
- 
-
   return (
     <>
-   <LoginWithFacebook/>
+      <Provider store={store}>
+        <LoginWithFacebook />
+      </Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+

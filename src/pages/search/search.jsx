7465118637dd74ebccc41331abd79b84e3dd42d7
Hady Awny <hadyawny5@gmail.com>
2024-07-30 20:35:00 +0300

    added search card and functionality

--- src/pages/search/search.jsx
+++ src/pages/search/search.jsx
@@ -1,28 +1,49 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { SearchAction } from "../../redux/slices/searchSlice.jsx";
+import SearchCardMUI from "../../components/searchCard/searchCardMUI.jsx";
 
-const Search = () => {
-    const { jobs, isLoading, error } = useSelector((state) => state.search);
-    const dispatch=useDispatch()
+const Search = ({ searchWord }) => {
+  const { jobs, isLoading, error } = useSelector((state) => state.search);
+  const [searchWords, setSearchWords] = useState(searchWord || "");
 
-    useEffect(()=>{
-        dispatch(SearchAction())
-    },[dispatch])
-    
-    console.log(jobs)
-    return (
-        <div>
-        <h1>Search</h1>
-        {isLoading && <p>Loading...</p>}
-        {error && <p>Error loading jobs.</p>}
-        <ul>
-          {jobs.map((job) => (
-            <li key={job._id}>{job.title}</li>
-          ))}
-        </ul>
-      </div>
-    );
-}
+  let baseSearchUrl = "https://jobboardbackend-u9zm.onrender.com/api/v1/jobs";
+
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setSearchWords(e.target.value);
+  };
+
+  useEffect(() => {
+    if (searchWord) {
+      dispatch(SearchAction(`${baseSearchUrl}?keyword=${searchWord}`));
+    }
+  }, [dispatch, baseSearchUrl, searchWord]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(SearchAction(`${baseSearchUrl}?keyword=${searchWords}`));
+  };
+
+  return (
+    <div>
+      <h1>Search</h1>
+      <form onSubmit={handleSubmit}>
+        {" "}
+        <input type="text" value={searchWords} onChange={handleChange} />{" "}
+        <button type="submit" disabled={isLoading}>
+          {isLoading ? "Searching..." : "Search"}
+        </button>{" "}
+      </form>
+
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error loading jobs.</p>}
+      {jobs.map((job) => (
+        <SearchCardMUI key={job.id} job={job}></SearchCardMUI>
+      ))}
+    </div>
+  );
+};
 
 export default Search;

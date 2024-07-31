9a950307a47a91f8b4688364c078475f8a5132e2
Omar Yasser <o.yasser.n@gmail.com>
2024-07-31 15:39:38 +0300

    last commit

--- src/components/profile/appliedJobsCard.jsx
+++ src/components/profile/appliedJobsCard.jsx
@@ -2,52 +2,32 @@ import * as React from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import ImageAvatars from "./avatar";
-import { Box } from "@mui/material";
-import FormDialog from "./formDialog";
+import { Box, Divider } from "@mui/material";
+import FeaturedJobsCard from "../FeaturedJobsCard";
 
 export default function MediaCard({ user }) {
   return (
     <Card sx={{ maxWidth: 900, position: "relative" }}>
-      <CardMedia
-        sx={{ height: 180 }}
-        image="/Profile/Canvas Design System Frame 53.png"
-        title="green iguana"
-      />
-      <Box
-        sx={{
-          position: "absolute",
-          top: 170, // This should be the same as your CardMedia height
-          // left: "50%",
-          transform: "translate(0%, -50%)",
-          zIndex: 1,
-          padding: "15px",
-        }}
-      >
-        {/* <ImageAvatars></ImageAvatars> */}
-      </Box>
-      <Box sx={{ position: "absolute", right: 0, margin: 2 }}>
-        <FormDialog user={user}></FormDialog>
-      </Box>
       <CardContent>
         <Box
           flexDirection={"column"}
           display={"flex"}
           alignItems={"flexStart"}
-          marginTop={5}
+          marginTop={1}
         >
           <Typography gutterBottom variant="h5" component="div">
-            {user.name}
+            Applied Jobs
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+            see below the list of your applied jobs
           </Typography>
         </Box>
       </CardContent>
+      <Divider variant="middle" />
+      <FeaturedJobsCard></FeaturedJobsCard>
       <CardActions>
         <Button size="small">Share</Button>
         <Button size="small">Learn More</Button>
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -4,6 +4,8 @@ import { getUsers, getUser } from "../redux/slices/userDetailsSlice";
 import { useDispatch, useSelector } from "react-redux";
 import MediaCard from "../components/profile/profileCard";
 import Variants from "../components/profile/skeleton";
+import AppliedJobsCard from "../components/profile/appliedJobsCard";
+import { Box, Container, Grid } from "@mui/material";
 
 let id = "66a541cd2ab3904d1fcbfee7";
 
@@ -20,9 +22,43 @@ export default function profile() {
     return <Variants></Variants>;
   }
   return (
-    <div>
-      <MediaCard user={usersArr[0]}></MediaCard>
-    </div>
+    // <div
+    //   style={{
+    //     display: "flex",
+    //     flexDirection: "column",
+    //     rowGap: "15px",
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //   }}
+    // >
+    //   <MediaCard user={usersArr[0]}></MediaCard>
+    //   <AppliedJobsCard user={usersArr[0]}></AppliedJobsCard>
+    //   <MediaCard user={usersArr[0]}></MediaCard>
+    //   <MediaCard user={usersArr[0]}></MediaCard>
+    // </div>
+
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        paddingY: 2,
+        gap: 2,
+      }}
+    >
+      <Box sx={{ width: "100%", maxWidth: 900 }}>
+        <MediaCard user={usersArr[0]} />
+      </Box>
+      <Box sx={{ width: "100%", maxWidth: 900 }}>
+        <AppliedJobsCard user={usersArr[0]} />
+      </Box>
+      <Box sx={{ width: "100%", maxWidth: 900 }}>
+        <MediaCard user={usersArr[0]} />
+      </Box>
+      <Box sx={{ width: "100%", maxWidth: 900 }}>
+        <MediaCard user={usersArr[0]} />
+      </Box>
+    </Container>
   );
 }
 
